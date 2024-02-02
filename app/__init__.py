e87a0a7c8c6fb3faba0b446477bba285bb9e63d3
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-02-02 11:19:31 -0500

    Add LANGCHAIN_TRACING_V2, LANGCHAIN_ENDPOINT, LANGCHAIN_API_KEY, and LANGCHAIN_PROJECT to __init__.py
    Remove app_config.py, langchain_config.py, llm_config.py, and tool_confi
    g.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,7 @@ lmstudio_api_key = llm_config.LMSTUDIO_API_KEY
 lmstudio_endpoint = llm_config.LMSTUDIO_ENDPOINT
 openai_api_key = llm_config.OPENAI_API_KEY
 
+LANGCHAIN_TRACING_V2="true"
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY=""
+LANGCHAIN_PROJECT="minio-weaviate-langchain"
--- app/config/langchain_config.py
+++
@@ -1,4 +0,0 @@
-# langchain_config.py
-# Configuration settings for LangChain
-
-LANGCHAIN_API_KEY = 'your_langchain_api_key'
