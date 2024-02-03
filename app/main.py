52a308829f86802cd818280ef917a681aaf2c76d
David <104325852+Cdaprod@users.noreply.github.com>
2024-02-03 16:25:38 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -89,4 +89,4 @@ async def delete_document(uuid: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
