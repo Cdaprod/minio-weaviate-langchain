3dcc4a4b52cb2fcb8d3b20947feb82f16c6442e0
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-02-07 22:29:51 -0500

    Update MINIO_ENDPOINT in tool_config.py

--- app/lib/config/tool_config.py
+++ app/lib/config/tool_config.py
@@ -8,7 +8,7 @@ WEAVIATE_API_KEY = ''
 # Configuration settings for MinIO
 
     
-MINIO_ENDPOINT = 'http://minio:9000'
+MINIO_ENDPOINT = 'minio:9000'
 MINIO_ACCESS_KEY = 'minio'
 MINIO_SECRET_KEY = 'minio123'
 MINIO_BUCKET = 'langchain-bucket'
